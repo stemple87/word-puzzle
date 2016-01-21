48a6fd516cef3001d01ad2c99b254f323afa3929
Ryan Brown and Simon Temple <browneryan87@gmail.com>
2016-01-21 11:17:09 -0800

    Add initial files.

---
+++ spec/specs.js
@@ -0,0 +1,9 @@
+
+
+
+
+// describe('getRemainder', function(){
+//   it("returns output array of counter added to itself until reaching input1 ", function(){
+//     expect(getRemainder(30, 5)).to.equal(5, 10, 15, 20, 25, 30);
+//   });
+// });
